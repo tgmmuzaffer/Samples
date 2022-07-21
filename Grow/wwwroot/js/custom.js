c104992e406d3c331fba67e96b3487f1c167b5be
muzaffer <muzaffer.deveci@tazebt.com>
2022-07-21 23:01:52 +0300

    Add project files.

---
+++ Grow/wwwroot/js/custom.js
@@ -0,0 +1,35 @@
+﻿$(window).scroll(function () {
+
+    var page = $(window),
+        cont = $('body'),
+        div = $('.change-color'),
+        grow = $('.grow'),
+        lr = $('.left-right');
+
+    var scroll = page.scrollTop() + (page.height() / 2);
+    var rounded = Math.floor(scroll);
+    var str_fontsize = rounded.toString();
+    if (str_fontsize.length == 3) {
+        str_fontsize = str_fontsize.substr(0, 2)
+    }
+    else if (str_fontsize.length == 4) {
+        str_fontsize = str_fontsize.substr(0, 3)
+    }
+    var num_fontsize = parseInt(str_fontsize);
+    var num__fontsize = 40 + num_fontsize;
+    var lr_size = ($(window).width() * (-1)) + scroll;
+    console.log(lr_size);
+    div.each(function () {
+        var $this = $(this);
+        if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
+
+            cont.removeClass(function (index, css) {
+                return (css.match(/(^|\s)color-\S+/g) || []).join(' ');
+            });
+            cont.addClass('color-' + $(this).data('color'));
+            grow.css("font-size", num__fontsize);
+            lr.css("left", lr_size)
+        }
+    });
+
+}).scroll();
---
+++ WebApplication1/Controllers/WeatherForecastController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+    //    "Keys": [
+    //  "210631193434-c6570275-a5c4-4851-810e-c1033e38c0f4",
+    //  "210615161378-4188fe46-3d81-4c2e-a657-efd2942d86c3"
+    //],
+    //"IpAddresses": [
+    //  "::1"
+    //]
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
